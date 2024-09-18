ea3db8050dce3456071479f09ef33d93e9eb9dbc
gzileni <giuseppe.zileni@gmail.com>
2024-09-18 18:13:38 +0200

    added library

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    trailingSlash: true,
-    // Target must be serverless
-    async rewrites() {
-        return [
-            {
-                source: '/data/:slug',
-                destination: 'https://d17kn6fj50jzfv.cloudfront.net/:slug',
-            }
-        ]
-    }
+    trailingSlash: true
 };
 
 export default nextConfig;
